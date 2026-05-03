75e4176f918f5e9f574dded90ccd47db384852f1
xiaoyuer5126 <2025201717@ruc.edu.cn>
2026-05-03 21:17:31 +0800

    修复topic时间戳的时区不匹配问题，alembic的env新增忽略 PostGIS 内部表

--- migrations/env.py
+++ migrations/env.py
@@ -53,11 +53,19 @@ def run_migrations_offline() -> None:
 
     """
     url = config.get_main_option("sqlalchemy.url")
+
+    def include_object(object, name, type_, reflected, compare_to):
+        # 忽略 PostGIS 内部表，避免自动迁移生成 drop/create 这些表
+        if type_ == "table" and name == "spatial_ref_sys":
+            return False
+        return True
+
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_object=include_object,
     )
 
     with context.begin_transaction():
@@ -77,9 +85,18 @@ def run_migrations_online() -> None:
         poolclass=pool.NullPool,
     )
 
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+    def include_object(object, name, type_, reflected, compare_to):
+        # 忽略 PostGIS 内部表，避免自动迁移生成 drop/create 这些表
+        if type_ == "table" and name == "spatial_ref_sys":
+            return False
+        return True
 
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            include_object=include_object,
+        )
         with context.begin_transaction():
             context.run_migrations()
 
