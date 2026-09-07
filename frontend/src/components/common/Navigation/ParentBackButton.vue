8db12e2a26b3a80318c338794e0165392356bf6a
芝士 <cheese@zhishi.local>
2026-09-07 12:11:39 -0400

    fix(navigation): use neutral color for parent links

--- frontend/src/components/common/Navigation/ParentBackButton.vue
+++ frontend/src/components/common/Navigation/ParentBackButton.vue
@@ -10,5 +10,7 @@ const backTo = computed(() =>
 </script>
 
 <template>
-  <v-btn v-if="backTo" :to="backTo" prepend-icon="mdi-arrow-left" variant="text" size="small"> 返回上一级 </v-btn>
+  <v-btn v-if="backTo" :to="backTo" prepend-icon="mdi-arrow-left" color="on-surface" variant="text" size="small">
+    返回上一级
+  </v-btn>
 </template>
